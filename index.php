c4e960cfb7b5839ed793d3e807b53e7664cd75da
shl0mo <salomao.dacostacruz@gmail.com>
2022-01-30 11:14:49 -0400

    novo_template-2

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 		
 		.titulo-sessao {
 			color: black;
-			font-size: 17pt;
+			font-size: 15pt;
 		}
 				
 		.ul-contato {
@@ -64,14 +64,14 @@
 			color: white;
 		}
 		
-		.container-secao {
+		.container-sessao {
 			padding-left: 45px;
 			padding-right: 35px;
 			margin-bottom: 5px;
 			margin-right: 280px;
 		}
 		
-		.descricao-secao {
+		.descricao-sessao {
 			text-align: justify;
 		}
 		
@@ -147,7 +147,7 @@
 			padding-top: 10px;
 		}
 		
-		.formacao {
+		.subtitulo-sessao {
 			margin-bottom: 5px;
 			font-weight: bold;
 		}
@@ -159,111 +159,32 @@
 		</style>");
 	$mpdf->WriteHTML("<body>");
 	$mpdf->WriteHTML("<div class='container-header'>
-			</div>");
+	</div>");
 	$mpdf->WriteHTML("<div class='container-nome'>
 		<div class='nome'>Salomão Cruz</div>
 		<span class='cargo o-m'>Estudante de Engenharia da Computação</span>
 	</div>");
-	$mpdf->WriteHTML("<div class='container-secao'><h2 class='titulo-sessao'>Perfil</h2><p class='descricao-secao'>Autodidata com gosto por desafios, vontade de aprender sem medir esforços pra evoluir e provido de um apurado senso analítico que me possibilita econtrar métodos e estratégias que levam à solução mais eficiente de cada problema</p></div>");
-	$mpdf->WriteHTML("<div class='container-secao'><h2 class='titulo-secao'>Formação Acadêmica</h2><div class='formacao'>Engenharia da Computação, Universidade Federal do Amazonas, Manaus</div><div class='periodos'>MARÇO 2019 - DEZEMBRO 2023</div></div>");
-	$mpdf->WriteHTML("<div class='container-secao'>
-						<h2 class='titulo-secao'>Competências</h2>
-						<ul>
-							<li>Apurada lógica de programação</li>
-							<li>Conhecimento avançado da linguagem Python</li>
-							<li>Conhecimento de bancos de dados relacionais e NoSQL</li>
-							<li>Desenvolvimento em Verilog</li>
-							<li>Frameworks front-end e back-end</li>
-							<li>Python para análise de dados - Pandas e Numpy</li>
-							<li>Analítica e resolução de problemas</li>
-							<li>Gestão de tempo</li>
-							<li>Inglês avançado</li>
-						</ul>
-					</div>");
-	$mpdf->WriteHTML("<div class='container-secao'><h2 class='titulo-secao'>Experiência Profissional</h2><span class='titulo-sub-secao'>Desenvolvedor Full Stack</span><span class='instituicao-curso' style='font-size: 14pt;'>, Agiliza Zap</span><span class='instituicao-curso' style='font-size: 14pt;'> &bull; 04/2021 - 09/2021<ul><li>Manutenção da API do software desenvolvida em Node.js</li><li>Manutenção do código-fonte do software em Visual Basic</li><li>Desenvolvimento de um painel de controle integrado, utilizando PHP com o framework Laravel</li></ul></div>");
-	$mpdf->WriteHTML("<div class='container-secao prox-pagina'><h2 class='titulo-secao'>Cursos Complementares</h2>
-		<ul>
-			<li class='titulo-sub-secao'>Introdução ao MongoDB e Bancos de Dados NoSQL, <span class='instituicao-curso'>Digital Innovation One Inc.</span></li>
-				<p class='descricao-curso'>Concluído em outubro de 2021<br>
-					<strong>Principais conceitos aprendidos:</strong>
-					<ul>
-						<li>Banco de dados não relacionais</li>
-						<li>Tipos de bancos de dados NoSQL</li>
-						<li>MongoDB</li>
-						<li>Schmea design</li>
-						<li>Operações de manipulação de dados</li>
-						<li>Performance e índices</li>
-						<li>Agregações</li>
-					</ul>
-				</p>
-			<li class='titulo-sub-secao p-t-20'>Curso Web Moderno Completo, <span class='instituicao-curso'>COD3R</span></li>
-			<p class='descricao-curso'>Concluído em agosto de 2021<br>
-				<strong>Principais conceitos aprendidos:</strong>
-				<ul>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>JavaScript</li>
-					<li>React.js</li>
-					<li>Vue.js</li>
-					<li>Bootstrap</li>
-					<li>Node.js</li>
-					<li>SQL</li>
-				</ul>
-			</p>
-			<li class='titulo-sub-secao p-t-20'>Artificial Intelligence A-Z&trade;: Learn How To Build an AI, <span class='instituicao-curso'>SuperDataScience</span></li>
-				<p class='descricao-curso'>Concluído em agosto de 2021<br><strong>Principais conceitos aprendidos:</strong>
-				<br> 
-				<ul>
-					<li>Redes Neurais - conceitos gerais</li>
-					<li>Q-Learning</li><li>Deep Q-Learning</li>
-					<li>Deep Convolutional Q-Learning</li>
-					<li>Redes Neurais Convolucionais</li>
-					<li>Advantage Asynchronous Actor-Critic</li>
-					<li>Long Short-Term Memory</li>
-				</ul>
-			</p>
-			<br>
-			<li class='titulo-sub-secao prox-pagina-sub'>Data Science Foundations, <span class='instituicao-curso'>VAI Academy</span></li>
-			<p class='descricao-curso'>Concluído em agosto de 2021<br><strong>Principais conceitos aprendidos:</strong><br> 
-				<ul>
-					<li>Conceitos gerais de Ciência de Dados</li>
-					<li>Algoritmos e estruturas de dados em Python</li>
-					<li>Análise de dados com Pandas</li>
-					<li>Numpy para análise de dados</li>
-					<li>Probabilidade e Estatística</li>
-				</ul>
-			</p>
-			<li class='titulo-sub-secao p-t-20'>Inteligência Artificial Fundamentos 2.0, <span class='instituicao-curso'>Data Science Academy</span></li>
-			<p class='descricao-curso'>Concluído em agosto de 2021<br><strong>Principais conceitos aprendidos:</strong><br> 
-				<ul>
-					<li>Filosofia da IA</li>
-					<li>Classificador K-NN</li>
-					<li>Tipos de aprendizado de máquina</li>
-					<li>Redes Adversárias Generativas</li>
-					<li>Implicações da IA na sociedade</li>
-				</ul>
-			</p>
-			<li class='titulo-sub-secao p-t-20'>Curso Desenvolvimento Web 2.0, <span class='instituicao-curso'>Danki Code</span></li>
-			<p class='descricao-curso'>Concluído em agosto de 2021<br><strong>Principais conceitos aprendidos:</strong><br> 
-				<ul>
-					<li>PSR4</li>
-					<li>Orientação a objetos em PHP</li>
-					<li>Composer e API's</li>
-					<li>CSS Avançado</li>
-					<li>Segurança em aplicações web</li>
-				</ul>
-			</p>
-			<li class='titulo-sub-secao p-t-20'>PHP Jedai, <span class='instituicao-curso'>Danki Code</span></li>
-			<p class='descricao-curso'>Concluído em junho de 2021<br><strong>Principais conceitos aprendidos:</strong><br> 
-				<ul>
-					<li>Linguagem PHP</li>
-					<li>Arquitetura MCV</li>
-					<li>Web Services e API</li>
-					<li>Laravel</li>
-					<li>MySQL no PHP</li>
-				</ul>
-			</p>
-			</ul>");
+	$mpdf->WriteHTML("<div class='container-sessao'>
+				<h2 class='titulo-sessao'>Perfil</h2>
+					<p class='descricao-sessao'>Autodidata com gosto por desafios, vontade de aprender sem medir esforços pra evoluir e provido de um apurado senso analítico que me possibilita econtrar métodos e estratégias que levam à solução mais eficiente de cada problema</p>
+	</div>");
+	$mpdf->WriteHTML("<div class='container-sessao'>
+	<h2 class='titulo-sessao'>Formação Acadêmica</h2>
+		<div class='subtitulo-sessao'>Engenharia da Computação, Universidade Federal do Amazonas, Manaus</div>
+		<div class='periodos'>Março 2019 - Dezembro 2023</div>
+	</div>");
+		$mpdf->WriteHTML("<div class='container-sessao'><h2 class='titulo-sessao'>Experiência Profissional</h2><div class='subtitulo-sessao'>Desenvolvedor Full Stack, Freelance, Manaus</div>
+			<div class='periodos'>Janeiro 2020 - Janeiro 2022</div>
+			<p class='descricao-sessao'>Trabalhei como desenvolvedor FullStack freelancer por dois anos. Desenvolvi diversos projetos para web, desde jogos, landing pages e sistemas completos. Pude aplicar meus conhecimentos de HTML5, CSS3, Javascript, PHP e em bibliotecas e frameworks, sobretudo de JavaScript</p>
+		</div>");
+	$mpdf->WriteHTML("<div class='container-sessao'><h2 class='titulo-sessao'>Cursos</h2>
+		<div class='subtitulo-sessao'>Bootcamp Eduzz FullStack Developer #2, Digital Innovation One.Inc</div>
+		<div class='periodos'>Dezembro 2021 - Janeiro 2022</div>
+		<div class='subtitulo-sessao'>Curso Web Moderno, COD3R</div>
+		<div class='periodos'>Junho 2021 - Julho 2021</div>
+		<div class='subtitulo-sessao'>Curso Desenvolvimento Web 2.0, Danki Code</div>
+		<div class='periodos'>Abril 2021 - Abril 2021</div>
+	</div>");
 	$mpdf->WriteHTML("</body>");
 	$mpdf->Output();
 ?>
