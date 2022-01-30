f63948d04a8f494d4719da1c6ce8026cc97b80dd
shl0mo <salomao.dacostacruz@gmail.com>
2022-01-30 09:16:12 -0400

    dependencias

--- curriculo.php
+++ curriculo.php
@@ -1,7 +1,7 @@
 <?php
-	include('vendor/autoload.php');
+	require_once __DIR__.'/vendor/autoload.php';
 	
-	$mpdf = new \Mpdf\Mpdf();
+	$mpdf = new Mpdf\Mpdf();
 	$mpdf->WriteHTML("<style>
 		* {
 			margin: 0;
